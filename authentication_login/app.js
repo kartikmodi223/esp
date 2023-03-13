02f9e5f888cfe7b25538df34535da3b938863a24
kartikmodi223 <kartik.modi.023@gmail.com>
2023-03-13 12:41:22 +0530

    ok

--- authentication_login/app.js
+++ authentication_login/app.js
@@ -371,6 +371,12 @@ app.get('/form', (req, res) => {
 app.post('/table', (req, res) => {
   // Retrieve the form data and insert it into the database
  
+
+  const jwtToken = req.cookies.jwtToken;
+  if (!jwtToken) {
+    return res.redirect("/login");
+  }
+
   const data = req.body;
   const course = req.body.course;
   const board = req.body.board;
@@ -686,6 +692,364 @@ app.get('/table', (req, res) => {
 });
 
 
+app.get('/edit', async (req, res) => {
+
+  const jwtToken = req.cookies.jwtToken;
+  if (!jwtToken) {
+    return res.redirect("/login");
+  }
+
+  var id = req.query.id;
+
+
+  db.query(`SELECT *from basic_info where is_deleted=0 and  id = ${id}`, (err, basic_info) => {
+    if (err) {
+      throw err;
+    }
+    db.query(`SELECT *from acadamics where  applicant_id = ${id}`, (err, acadamics) => {
+      if (err) {
+        throw err;
+      }
+      db.query(`SELECT *from work_experience where  applicant_id = ${id}`, (err, work_experience) => {
+        if (err) {
+          throw err;
+        }
+        db.query(`SELECT *from reference where  applicant_id = ${id}`, (err, reference) => {
+          if (err) {
+            throw err;
+          }
+          db.query(`SELECT *from preference where  applicant_id = ${id}`, (err, preference) => {
+            if (err) {
+              throw err;
+            }
+            db.query(`SELECT *from LanguagesKnown where  applicant_id = ${id}`, (err, lan) => {
+              if (err) {
+                throw err;
+              }
+              db.query(`SELECT *from skills where  applicant_id = ${id}`, (err, skills) => {
+                if (err) {
+                  throw err;
+                }
+
+                db.query('SELECT *from state_master', (err, states) => {
+                  if (err) {
+                    throw err;
+                  }
+                  // const stateId = req.params.stateId;
+                  // db.query('SELECT * FROM city_master WHERE state_id = ?', [stateId], (error, results) => {
+                  //   if (error) {
+                  //     console.log(error);
+                  //   } else {
+                  //     res.json(results);
+                  //   }
+                  db.query('SELECT option_name FROM option_master where select_id = 6', (err, rel) => {
+                    if (err) {
+                      throw err;
+                    }
+                    db.query('SELECT option_name FROM option_master where select_id = 2', (err, location) => {
+                      if (err) {
+                        throw err;
+                      }
+
+                      db.query('SELECT option_name FROM option_master where select_id = 7', (err, department) => {
+                        if (err) {
+                          throw err;
+                        }
+                        db.query('SELECT option_name FROM option_master where select_id = 3', (err, courses) => {
+                          if (err) {
+                            throw err;
+                          }
+                          db.query('SELECT option_name FROM option_master where select_id = 4', (err, languages) => {
+                            if (err) {
+                              throw err;
+                            }
+                            db.query('SELECT option_name FROM option_master where select_id = 5', (err, technologies) => {
+                              if (err) {
+                                throw err;
+                              }
+                              db.query(`select * from LanguagesKnown where applicant_id = ${id}`, (err, lang) => {
+                                if (err) throw err;
+                                var lanjson = JSON.stringify(lang);
+                                console.log(lang)
+                                console.log("lang" + lanjson);
+
+
+
+
+                                // Render the job application form and pass the data for the select boxes to the template
+                                res.render('edit', { basic_info, reference, lanjson, lang, preference, acadamics, work_experience, skills, states, relation: rel, location: location, department: department, course: courses, language: languages, tec: technologies });
+                                console.log(basic_info)
+                                console.log(acadamics)
+                                console.log(work_experience)
+                                console.log(reference)
+                                console.log(preference)
+                                // console.log(results)
+                              });
+                            });
+                          });
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+// });
+
+
+app.post('/update', function (req, res, next) {
+
+  const jwtToken = req.cookies.jwtToken;
+  if (!jwtToken) {
+    return res.redirect("/login");
+  }
+
+  const data = req.body;
+  const id = data.id;
+  var query = `
+	UPDATE basic_info 
+	SET first_name ="${data.first_name}", 
+	last_name ="${data.last_name}", 
+	email ="${data.email}", 
+	gender ="${data.gender}" ,
+  address1 ="${data.address1}",
+  job_designation ="${data.job_designation}",
+  phone ="${data.phone}" ,
+  zip ="${data.zip}",
+  relation_status ="${data.relation_status}"
+	WHERE id = ${data.id}
+	`;
+
+  const basicValue = [data.first_name, data.last_name, data.gender, data.dob,
+  data.job_designation, data.address1, data.email, data.phone, data.city, data.states, data.zip,
+  data.relation_status];
+  console.log(query)
+  db.query(query, function (error, data) {
+    if (error) {
+      throw error;
+    }
+    else {
+      res.send("ok")
+    }
+
+    const course = req.body.course;
+    const board = req.body.board;
+    const passingyear = req.body.passingYear;
+    const pr = req.body.percentage;
+
+    db.query(`delete from acadamics where applicant_id=${id}`, (err, result) => {
+      if (err) throw err;
+      console.log("edu deleted");
+    });
+    applicantId = id;
+
+    if (typeof (course, board, passingyear, pr) == "string") {
+      eduSql = `insert into acadamics(applicant_id,course,board,passingYear,percentage) values
+    ('${applicantId}','${course}','${board}','${passingyear}','${pr}')`;
+
+      db.query(eduSql, (err, result) => {
+        if (err) throw err;
+        console.log("edu inserted");
+      });
+    } else {
+      for (i = 0; i < course.length; i++) {
+        eduSql = `insert into acadamics(applicant_id,course,board,passingYear,percentage) values
+    ('${applicantId}','${course[i]}','${board[i]}','${passingyear[i]}','${pr[i]}')`;
+
+        db.query(eduSql, (err, result) => {
+          if (err) throw err;
+          console.log("edu inserted");
+        });
+      }
+    }
+
+
+
+
+    db.query(`delete from work_experience where applicant_id = ${id}`, (err, result) => {
+      if (err) throw err;
+      console.log("work experience deleted!");
+    })
+    const c_name = req.body.company_name;
+    const desig = req.body.jobtitle;
+    const start = req.body.start_date;
+    const end = req.body.end_date;
+    console.log(c_name);
+    console.log(desig);
+    console.log(start);
+    console.log(end);
+    if (typeof (c_name, desig, start, end) == "string") {
+      expSql = `insert into work_experience(applicant_id,company_name,jobtitle,start_date,end_date) values
+    ('${applicantId}','${c_name}','${desig}','${start}','${end}')`;
+
+      db.query(expSql, (err, result) => {
+        if (err) throw err;
+        console.log("exp inserted");
+      });
+    } else {
+      for (i = 0; i < c_name.length; i++) {
+        expSql = `insert into work_experience(applicant_id,company_name,jobtitle,start_date,end_date) values
+    ('${applicantId}','${c_name[i]}','${desig[i]}','${start[i]}','${end[i]}')`;
+
+        db.query(expSql, (err, result) => {
+          if (err) throw err;
+          console.log("exp inserted");
+        });
+      }
+    }
+    db.query(`delete from work_experience where applicant_id = ${id}`, (err, result) => {
+      if (err) throw err;
+      console.log("work experience deleted!");
+    })
+
+    console.log(desig);
+    console.log(start);
+    console.log(end);
+    if (typeof (c_name, desig, start, end) == "string") {
+      expSql = `insert into work_experience(applicant_id,company_name,jobtitle,start_date,end_date) values
+    ('${applicantId}','${c_name}','${desig}','${start}','${end}')`;
+
+      db.query(expSql, (err, result) => {
+        if (err) throw err;
+        console.log("exp inserted");
+      });
+    } else {
+      for (i = 0; i < c_name.length; i++) {
+        expSql = `insert into work_experience(applicant_id,company_name,jobtitle,start_date,end_date) values
+    ('${applicantId}','${c_name[i]}','${desig[i]}','${start[i]}','${end[i]}')`;
+
+        db.query(expSql, (err, result) => {
+          if (err) throw err;
+          console.log("exp inserted");
+        });
+      }
+    }
+
+    db.query(`delete from LanguagesKnown where applicant_id=${id}`, (err, result) => {
+      if (err) throw err;
+    });
+    var applicantId = id;
+    var lang = req.body.Language;
+    var r = req.body[lang + "read"] ? "yes" : "no";
+    var w = req.body[lang + "write"] ? "yes" : "no";
+    var s = req.body[lang + "speak"] ? "yes" : "no";
+    console.log(lang);
+    console.log(r);
+    if (typeof lang == "string") {
+      var query_lan =
+        "INSERT INTO LanguagesKnown(applicant_id,Language,`read`,`write`,`speak`) VALUES (?, ?, ?, ?, ?)";
+      db.query(query_lan, [applicantId, lang, r, w, s], (err, ans) => {
+        if (err) return console.log(err.message);
+        console.log("languages inserted");
+      });
+    } else {
+      lang.forEach((language) => {
+        const read2 = req.body[language + "read"] ? "yes" : "no";
+        const write2 = req.body[language + "write"] ? "yes" : "no";
+        const speak2 = req.body[language + "speak"] ? "yes" : "no";
+
+        db.query(
+          "INSERT INTO LanguagesKnown(applicant_id,Language,`read`,`write`,`speak`) VALUES (?, ?, ?, ?, ?)",
+          [applicantId, language, read2, write2, speak2],
+          (err, result) => {
+            if (err) {
+              throw err;
+            }
+
+            console.log("language inserted");
+          }
+        );
+      });
+    }
+
+    //getting technology
+    db.query(`delete from skills where applicant_id = ${id}`, (err, result) => {
+      if (err) throw err;
+    });
+    const skills = req.body.technology;
+    const lavel = req.body[skills + "a"];
+    console.log("skills " + skills);
+    if (typeof skills == "string") {
+      db.query(
+        "insert into skills(applicant_id,technology,lavel) values(?,?,?)",
+        [applicantId, skills, lavel],
+        (err, result) => {
+          if (err) throw err;
+          console.log("skills one inserted");
+        }
+      );
+    } else {
+      skills.forEach((tec) => {
+        const lavel = req.body[tec + "a"];
+        console.log("lavel " + lavel);
+        db.query(
+          "insert into skills(applicant_id,technology,lavel) values(?,?,?)",
+          [applicantId, tec, lavel],
+          (err, result) => {
+            if (err) throw err;
+            console.log("skills inserted");
+          }
+        );
+      });
+    }
+  });
+});
+
+
+app.get('/combobox',async (req,res)=>{
+
+      
+  var combo0 = await generateCombo('technologies');      
+  var combo1 = await generateCombo('languages');      
+  var combo2 = await generateCombo('courses');    
+  var combo3 = await generateCombo('department');
+  var combo4 = await generateCombo('relation');      
+  var combo5 = await generateCombo('state');      
+  var combo6 = await generateCombo('location');    
+  var combo7 = await generateCombo('gender');   
+
+  res.render('combobox',{technologies: combo0,languages:combo1,courses:combo2,department:combo3,relation:combo4,state:combo5,location:combo6,gender:combo7});
+
+});
+
+  async function generateCombo(combo){
+
+      var comboname =  combo;
+   
+   var query2 = `select option_name,option_master.option_id from option_master join select_master on option_master.select_id = select_master.select_id where select_master.select_name ='${comboname}';`
+   var data = await getdata(query2);
+   
+   console.log(data);
+   
+   
+   var comboStr = "";
+   comboStr += `<lable for='${comboname}'>${comboname}</lable><select id='${comboname}' name='${comboname}'>`;
+   
+   for(let i=0;i<data.length;i++){
+    comboStr+=`<option value='${data[i].id}' >${data[i].option_name}</option>`;
+   }
+   
+   comboStr += `</select>`;
+    return comboStr;
+   
+   }
+   
+   function getdata(query){
+       return new Promise((resolve,reject)=>{
+           db.query(query,(err,result)=>{
+               if(err) throw err;
+               resolve(result);
+           });
+       });
+   }
+
+
 
 const con1 = mysql.createConnection({
   host: "localhost",
@@ -770,13 +1134,40 @@ app.get('/searcht', (req, res) => {
 
 })
 
+app.get('/:page',(req,res)=>{
 
+  const jwtToken = req.cookies.jwtToken;
+  if (!jwtToken) {
+    return res.redirect("/login");
+  }
+
+  let page =parseInt(req.params.page) || 1;
+  let limit =100;
+  let offset=(page - 1)*limit;
+  
+
+      con1.query(`select * from student_express  LIMIT ${offset}, ${limit} `,(err,result)=>{
+          if (err) throw err;
+          // res.render('table',{data:result})
+  
+   con1.query(`select count(*) as count  from student_express`,(err,outcome)=>{
+          if (err) throw err;
+  
+     let totalpages = Math.ceil(outcome[0].count/limit);
+     let pages =[];
+     for(let i=1; i<=totalpages;i++){
+      pages.push(i);
+     }
+          res.render('pagination',{data:result,page:page,pages:pages});
+      });
+    });
+  });
 
 
 app.get('/tictactoe', (req, res) => {
   const jwtToken = req.cookies.jwtToken;
   if (!jwtToken) {
-    return res.redirect("/home");
+    return res.redirect("/login");
   }
   res.sendFile("/home/kartik-modi/kartik_modi/authentication_login/public/html/tictactoe.html");
 });
@@ -784,10 +1175,12 @@ app.get('/tictactoe', (req, res) => {
 app.get('/kuku', (req, res) => {
   const jwtToken = req.cookies.jwtToken;
   if (!jwtToken) {
-    return res.redirect("/home");
+    return res.redirect("/login");
   }
   res.sendFile("/home/kartik-modi/kartik_modi/authentication_login/public/html/color_change_qube.html");
 });
+
+
 
 
 
